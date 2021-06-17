465bd11a31be472a99a362525bbf3c884b991610
SpiderMath <71999854+SpiderMath@users.noreply.github.com>
2021-06-17 16:58:09 +0530

    Finished coding

---
+++ Source/Game.java
@@ -0,0 +1,48 @@
+package Source;
+import java.util.Scanner;
+
+class Game {
+	public static void main(String[] args) {
+		int lowerlimit, upperlimit;
+		if(args.length == 0) {
+			lowerlimit = 1;
+			upperlimit = 10;
+		}
+		else if(args.length == 1) {
+			lowerlimit = Integer.parseInt(args[0]);
+			upperlimit = 10;
+		}
+		else {
+			lowerlimit = Integer.parseInt(args[0]);
+			upperlimit = Integer.parseInt(args[1]);
+		}
+
+		if(lowerlimit < 0 || upperlimit < 0) System.out.println("Either of the limits is negative");
+		else Game.logic(lowerlimit, upperlimit);
+	}
+
+	public static void logic(int lowerlimit, int upperlimit) {
+		java.util.Random rand = new java.util.Random();
+		int random = rand.nextInt(upperlimit - lowerlimit) + lowerlimit;
+		Scanner myScanner = new Scanner(System.in);
+
+		System.out.println("A random number between " + lowerlimit + " and " + upperlimit + " has been chosen. You have 5 tries to guess the correct answer");
+		for(int i = 0; i < 5; i++) {
+			int input = myScanner.nextInt();
+
+			if(input == random) {
+				System.out.println("You guessed the correct number! You win!");
+				break;
+			}
+			else {
+				if(input > random) System.out.println("The number is less than the given number");
+				else System.out.println("The number is more than the given number");
+
+				if(i == 4) System.out.println("You lost! The number was " + random);
+				else System.out.println("Number of tries left: " + (4 - i));
+			}
+		}
+
+		myScanner.close();
+	}
+}
